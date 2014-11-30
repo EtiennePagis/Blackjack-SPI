241c5eac700e3e51b42525535c27622e35f1eee0
twixbadevil <victorien.grude@univ-lemans.fr>
2014-11-30 11:36:46 +0100

    test debut

---
+++ projet_test.c
@@ -0,0 +1,12 @@
+//// Fichier projet_test
+//// Created by Pagis Etienne, Fouquet Stevy and Grudé Victorien
+#include "CUnit/CUnit.h"
+#include "CUnit/Basic.h"
+#include "CUnit/Automated.h"
+#include "CUnit/Console.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "mains.h"
+#include "carte.h"
+#include "score.h"
